e193fb4329af6fe7d35c8f68e243d42ac19264ba
Mikko Rauhala <mikko@rauhala.net>
2017-06-04 08:12:28 +0300

    Updated radar coordinates.

--- radars-finland.js
+++ radars-finland.js
@@ -1,64 +1,64 @@
 // MeteoFI / Mikko Rauhala / 2017
 // Radar Configuration for Finland
 var radars = {
-    anjalankoski: {
-	name: "Anjalankoski",
-	acronym: "ANJ",
-	country: "fi",
-	location: {lat: 60.9036, lng: 27.1111}
-    },
-    ikaalinen: {
-	name: "Ikaalinen",
-	acronym: "IKA",
-	country: "fi",
-	location: {lat: 61.7671, lng: 23.0799}
-    },
-    kesalahti: {
-	name: "Kesälahti",
-	acronym: "KES",
-	country: "fi",
-	location: {lat: 61.9070, lng: 29.7977}
-    },
-    korpo: {
-	name: "Korpo",
-	acronym: "KOR",
-	country: "fi",
-	location: {lat: 60.1284, lng: 21.6465}
-    },
-    kuopio: {
-	name: "Kuopio",
-	acronym: "KUO",
-	country: "fi",
-	location: {lat: 62.8624, lng: 27.3854}
-    },
-    luosto: {
-	name: "Luosto",
-	acronym: "LUO",
-	country: "fi",
-	location: {lat: 67.1386, lng: 26.9008}
-    },
-    petajavesi: {
-	name: "Petäjävesi",
-	acronym: "PET",
-	country: "fi",
-	location: {lat: 62.3045, lng: 25.4401}
-    },
-    utajarvi: {
-	name: "Utajärvi",
-	acronym: "UTA",
-	country: "fi",
-	location: {lat: 64.7745, lng: 26.3225}
-    },
-    vantaa: {
-	name: "Vantaa",
-	acronym: "VAN",
-	country: "fi",
-	location: {lat: 60.27062, lng: 24.86902}
-    },
-    vimpeli: {
-	name: "Vimpeli",
-	acronym: "VIM",
-	country: "fi",
-	location: {lat: 63.1046, lng: 23.8246}
-    },
+	anjalankoski: {
+		name: "Anjalankoski",
+		acronym: "ANJ",
+		country: "fi",
+		location: { lat: 60.9040, lng: 27.1082 }
+	},
+	ikaalinen: {
+		name: "Ikaalinen",
+		acronym: "IKA",
+		country: "fi",
+		location: { lat: 61.7675, lng: 23.0768 }
+	},
+	kesalahti: {
+		name: "Kesälahti",
+		acronym: "KES",
+		country: "fi",
+		location: { lat: 61.9070, lng: 29.7977 }
+	},
+	korpo: {
+		name: "Korppoo",
+		acronym: "KOR",
+		country: "fi",
+		location: { lat: 60.1285, lng: 21.6434 }
+	},
+	kuopio: {
+		name: "Kuopio",
+		acronym: "KUO",
+		country: "fi",
+		location: { lat: 62.8626, lng: 27.3815 }
+	},
+	luosto: {
+		name: "Luosto",
+		acronym: "LUO",
+		country: "fi",
+		location: { lat: 67.1391, lng: 26.8969 }
+	},
+	petajavesi: {
+		name: "Petäjävesi",
+		acronym: "PET",
+		country: "fi",
+		location: { lat: 62.3045, lng: 25.4401 }
+	},
+	utajarvi: {
+		name: "Utajärvi",
+		acronym: "UTA",
+		country: "fi",
+		location: { lat: 64.7749, lng: 26.3189 }
+	},
+	vantaa: {
+		name: "Vantaa",
+		acronym: "VAN",
+		country: "fi",
+		location: { lat: 60.2706, lng: 24.8690 }
+	},
+	vimpeli: {
+		name: "Vimpeli",
+		acronym: "VIM",
+		country: "fi",
+		location: { lat: 63.1048, lng: 23.8209 }
+	},
 };
